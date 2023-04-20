3331e596f0983f7cdca9e19e2eb0091d8bcc90b4
Ryanhu-hub <ryanhuot20@gmail.com>
2023-04-19 19:08:52 -0700

    added shows.jsx

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT
 const app = express()
 
 // MIDDLEWARE
+app.use(express.static('public'))
+
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
@@ -24,4 +26,10 @@ app.listen(PORT, () => {
   // Breads
   const breadsController = require('./controllers/breads_controller.js')
   app.use('/breads', breadsController)
+
+  // 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
+
   
