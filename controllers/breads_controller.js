da530206828eb3dabdfa62c6e0ff7377625c72d4
Ryanhu-hub <ryanhuot20@gmail.com>
2023-04-22 11:46:31 -0700

    added new edit and delete buttons for pages in breads_controller

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,7 +4,7 @@ const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-    res.render('index',
+    res.render('Index',
     {
         breads: Bread
     }
@@ -18,14 +18,16 @@ breads.get('/new', (req, res) => {
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
-    if (Bread[req.params.arrayIndex]) {
-      res.render('Show', {
-        bread:Bread[req.params.arrayIndex]
-      })
-    } else {
-      res.send('404')
-    }
-  })
+  if (Bread[req.params.arrayIndex]) {
+    res.render('Show', {
+      bread:Bread[req.params.arrayIndex],
+      index: req.params.arrayIndex,
+    })
+  } else {
+    res.render('404')
+  }
+})
+
   
 // CREATE
 breads.post('/', (req, res) => {
@@ -41,7 +43,31 @@ breads.post('/', (req, res) => {
     res.redirect('/breads')
   })
   
-  
-  
+  // DELETE
+breads.delete('/:indexArray', (req, res) => {
+  Bread.splice(req.params.indexArray, 1)
+  res.status(303).redirect('/breads')
+})
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
+})
+
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+})
+
+
 module.exports = breads
 
--- views/index.jsx
+++ views/index.jsx
@@ -29,3 +29,4 @@ function Index ({breads})  {
 }
 
 module.exports = Index
+
